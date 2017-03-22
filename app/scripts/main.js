1c33f44a939e242b86654c578185ea9c4298052d
Amanda Schlaudt <aschlaudt@Amandas-MacBook-Pro.local>
2017-03-22 17:14:04 -0500

    Adding styles for the class card

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,17 @@ $('#page-header').load('../components/navbar.html');
 $('#body').load('../components/body.html');
 $('#page-footer').load('../components/footer.html');
 
+
+window.onload = function () {
+    var list = document.getElementsByClassName('class-card');
+
+    for (var i = 0; i < list.length; i++) {
+        var src = list[i].getAttribute('data-image-src');
+        list[i].style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url('" + src + "')";
+    }
+}
+
+
 // $(function() {
 //   $(document).click(function (event) {
 //     $('.navbar-collapse').collapse('hide');
